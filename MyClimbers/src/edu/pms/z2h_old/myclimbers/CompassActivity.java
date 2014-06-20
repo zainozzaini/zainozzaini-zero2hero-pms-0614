1ac9cc645bae9298e6fffa96f54ed73903c11ace
zainoz_zaini <zainoz.zaini@gmail.com>
2014-06-20 14:49:25 +0800

    Update compass by using Rotation Sensor

--- MyClimbers/src/edu/pms/z2h_old/myclimbers/CompassActivity.java
+++ MyClimbers/src/edu/pms/z2h_old/myclimbers/CompassActivity.java
@@ -1,6 +1,11 @@
 package edu.pms.z2h_old.myclimbers;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,32 +21,40 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CompassActivity extends Activity{
+public class CompassActivity extends Activity implements SensorEventListener{
 	private TextView txtRotate;
 	private Button btnRotate;
 	private LinearLayout linearCompass;
 	private EditText editRotate;
 	private ImageView imgCompass;
+
+	private SensorManager mSensorManager;
+	private Sensor mOrientation;
 	
+	private float prevRotation = 0f;
+
 	private String tag = getClass().getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compass);
-		
+
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
 		btnRotate =  (Button) findViewById(R.id.btnRotate);
 		txtRotate = (TextView) findViewById(R.id.txtRotate);
 		linearCompass = (LinearLayout) findViewById(R.id.layoutCompass);
 		editRotate  = (EditText) findViewById(R.id.editRotate);
 		imgCompass = (ImageView)findViewById(R.id.imgCompass);
-		
-		
-		
-		
-		
-		
-		
+
+
+
+
+
+
+
 		btnRotate.setOnLongClickListener(new OnLongClickListener(){
 
 			@Override
@@ -50,27 +63,27 @@ public class CompassActivity extends Activity{
 				Log.i(tag,txtRotate.getText().toString());	
 				txtRotate.setText(editRotate.getText().toString());
 				Log.i(tag,txtRotate.getText().toString());
-				
+
 				//Demo 4 Task 
 				try{
-//		:P			imgCompass.setRotation(Float.parseFloat(txtRotate.getText().toString()));
+					//		:P			imgCompass.setRotation(Float.parseFloat(txtRotate.getText().toString()));
 					rotateAnimation(Float.parseFloat(txtRotate.getText().toString()));
 				}catch(Exception e){
 					Toast.makeText(getApplication(), "Invalid Value", Toast.LENGTH_LONG).show();
 				}
-				
-				
-				
-				
+
+
+
+
 				return false;
 			}});
 
 
 	}
-	
-	
+
+
 	private void rotateAnimation(float currentRotation){
-		RotateAnimation anim = new RotateAnimation(0f, currentRotation,imgCompass.getWidth()/2, imgCompass.getHeight()/2);
+		RotateAnimation anim = new RotateAnimation(prevRotation, currentRotation,imgCompass.getWidth()/2, imgCompass.getHeight()/2);
 		anim.setInterpolator(new LinearInterpolator());
 		anim.setRepeatCount(0);
 		anim.setDuration(1500);
@@ -78,12 +91,45 @@ public class CompassActivity extends Activity{
 
 		// Start animating the image
 		imgCompass.startAnimation(anim);
+		
+		prevRotation= currentRotation;
 
-		
-		
+
+
 	}
-	
-	
-	
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
+	}
+
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float azimuth_angle = event.values[0];
+		float pitch_angle = event.values[1];
+		float roll_angle = event.values[2];
+		// Do something with these orientation angles.
+		
+		Log.i(tag,"North "+ azimuth_angle);
+		rotateAnimation(azimuth_angle);
+	}
+
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+
 
 }
